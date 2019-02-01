c0c929b0183682251e32d9fddb1ef8cc0ab2afe4
= <=>
2019-02-02 04:10:24 +0800

    Enabled an option for twitch chat on the side, allowing user to set if an additional parameter renders chat

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 var hls;
 var debug;
+var twitch_enable;
 var recoverDecodingErrorDate,recoverSwapAudioCodecDate;
 var pendingTimedMetadata = [];
 
@@ -41,15 +42,32 @@ function timeUpdateCallback() {
   console.log('Metadata ' + e.value + " at " + e.pts + "s");
 }
 
-function playM3u8(url){
+function playM3u8(abs_path){
+  var url = abs_path.split('#')[1];
+  var twitch = abs_path.split('#')[2];
   var video = document.getElementById('video');
+  var chat_div = document.getElementById("chat");
+  chat_div.innerHTML = "";
+
   if(native){
     video.classList.add("native_mode");
-    video.classList.remove("zoomed_mode");
+    video.classList.remove("zoomed_mode");;
   } else {
     video.classList.remove("native_mode");
     video.classList.add("zoomed_mode");
   }
+
+  if (twitch_enable == true && twitch != undefined && typeof twitch == "string"){
+    video.className = "twitch_mode";
+    var chat_iframe = document.createElement('iframe');
+    chat_iframe.src = "https://www.twitch.tv/embed/" + twitch + "/chat";
+    chat_iframe.scrolling = "yes";
+    chat_iframe.frameBorder = "0";
+    chat_iframe.id = "chat_embed";
+    chat_iframe.width = "100%";
+    chat_div.append(chat_iframe);
+  }
+
   if(hls){ hls.destroy(); }
   hls = new Hls({debug:debug});
   hls.on(Hls.Events.ERROR, function(event,data) {
@@ -84,20 +102,23 @@ function playM3u8(url){
 chrome.storage.local.get({
   hlsjs: currentVersion,
   debug: false,
-  native: false
+  native: false,
+  twitch_enable: true
 }, function(settings) {
   debug = settings.debug;
   native = settings.native;
+  twitch_enable = settings.twitch_enable;
   var s = document.createElement('script');
   var version = currentVersion
   if (supportedVersions.includes(settings.hlsjs)) {
     version = settings.hlsjs
   }
   s.src = chrome.runtime.getURL('hlsjs/hls.'+version+'.min.js');
-  s.onload = function() { playM3u8(window.location.href.split("#")[1]); };
+  // console.log(window.location.href.split("#"))
+  s.onload = function() { playM3u8(window.location.href); };
   (document.head || document.documentElement).appendChild(s);
 });
 
 $(window).bind('hashchange', function() {
-  playM3u8(window.location.href.split("#")[1]);
+  playM3u8(window.location.href);
 });
