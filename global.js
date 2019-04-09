6fa455df226e8991bbc593f05baa945571583f63
Gaetan Hervouet <gaetan.hervouet@gmail.com>
2019-04-08 20:48:35 -0400

    hls.js 0.12.4

--- global.js
+++ global.js
@@ -1,2 +1,2 @@
-var supportedVersions = [ "0.10.1", "0.9.1", "0.8.9","0.8.8","0.8.7","0.8.6","0.8.5","0.7.10", "0.6.21", "0.5.52"]
-var currentVersion = supportedVersions[1]
+var supportedVersions = ["0.12.4", "0.10.1", "0.9.1", "0.8.9","0.8.8","0.8.7","0.8.6","0.8.5","0.7.10", "0.6.21", "0.5.52"]
+var currentVersion = supportedVersions[0]
